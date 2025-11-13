943be060f81e341d73bd441d5f3ba89093508c91
Jply <josecabeza.compras@gmail.com>
2025-11-14 00:12:29 +0100

    1.1.0 MODO OSCURO OK

--- app/home/index.jsx
+++ app/home/index.jsx
@@ -12,7 +12,7 @@ import UpdateAdvisor from "components/UpdateAdvisor";
 export default function HomeScreen() {
 
 
-  const {nowPlayingQuery, popularQuery, topRatedQuery, upcommingQuery} = useMovies();
+  const { nowPlayingQuery, popularQuery, topRatedQuery, upcommingQuery } = useMovies();
 
   if (nowPlayingQuery.isLoading && popularQuery.isLoading && topRatedQuery.isLoading && upcommingQuery.isLoading) {
     return (
@@ -24,26 +24,26 @@ export default function HomeScreen() {
 
   return (
     <>
-    <UpdateAdvisor />
-    <SafeAreaView className="bg-gray-100">
-      
-      <View className="p-4 m-3 border-b-2 pb-2 flex flex-row justify-between items-center rounded-lg">
-         <Text className="text-5xl font-bold text-red-600">
-        PelisWix
-      </Text>
-      <Pressable
-                onPress={() => router.push("/settings/settings")}>
-                <Ionicons name="settings" size={40}
-                  color="black"
-                  className="shadow" />
-              </Pressable>
-      </View>
-       <MainSlideShow movies={nowPlayingQuery.data?.pages.flat()} loadNextPage={nowPlayingQuery.fetchNextPage}/> 
-     <MovieHorizontalList movies={popularQuery.data?.pages.flat()} title={"Populares"} loadNextPage={popularQuery.fetchNextPage} />
-     <MovieHorizontalList movies={topRatedQuery.data?.pages.flat()} title={"Mejor valoradas"} loadNextPage={topRatedQuery.fetchNextPage} />
-     <MovieHorizontalList movies={upcommingQuery.data?.pages.flat()} title={"Proximos lanzamientos"} loadNextPage={upcommingQuery.fetchNextPage} />
+      <UpdateAdvisor />
+      <SafeAreaView className="bg-gray-100">
 
-    </SafeAreaView>
+        <View className="p-4 m-3 border-b-2 pb-2 flex flex-row justify-between items-center rounded-lg">
+          <Text className="text-5xl font-bold text-red-600">
+            PelisWix
+          </Text>
+          <Pressable
+            onPress={() => router.push("/settings/settings")}>
+            <Ionicons name="settings" size={40}
+              color="black"
+              className="shadow" />
+          </Pressable>
+        </View>
+        <MainSlideShow movies={nowPlayingQuery.data?.pages.flat()} loadNextPage={nowPlayingQuery.fetchNextPage} />
+        <MovieHorizontalList movies={popularQuery.data?.pages.flat()} title={"Populares"} loadNextPage={popularQuery.fetchNextPage} />
+        <MovieHorizontalList movies={topRatedQuery.data?.pages.flat()} title={"Mejor valoradas"} loadNextPage={topRatedQuery.fetchNextPage} />
+        <MovieHorizontalList movies={upcommingQuery.data?.pages.flat()} title={"Proximos lanzamientos"} loadNextPage={upcommingQuery.fetchNextPage} />
+
+      </SafeAreaView>
     </>
   );
 }
